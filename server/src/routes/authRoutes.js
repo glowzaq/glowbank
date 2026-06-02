0387e160ea659b8610f2a1939015a8177d9d25c9
Inioluwa Akintokun <inioluwaakintokun@gmail.com>
2026-06-02 08:18:36 +0100

    chore:remove unused updateProfile import

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { register, login, updateProfile } from '../controllers/authController.js'
+import { register, login } from '../controllers/authController.js'
 import { authLimiter } from '../middleware/rateLimit.js'
 
 
